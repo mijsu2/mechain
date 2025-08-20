3654d6ccf74ab57928f58a5609cc0ab7571f9663
Ailouxxe <abachoco@paterostechnologicalcollege.edu.ph>
2025-08-20 12:10:14 +0800

    Removed Base44 login redirect

--- src/api/base44Client.js
+++ src/api/base44Client.js
@@ -1,8 +1,9 @@
-import { createClient } from '@base44/sdk';
+//import { createClient } from '@base44/sdk';
 // import { getAccessToken } from '@base44/sdk/utils/auth-utils';
 
 // Create a client with authentication required
-export const base44 = createClient({
-  appId: "6880a09bdee873c356e7b267", 
-  requiresAuth: true // Ensure authentication is required for all operations
-});
+//export const base44 = createClient({
+  //appId: "6880a09bdee873c356e7b267", 
+  //requiresAuth: true // Ensure authentication is required for all operations
+//});
+<App />
